d10c3cc95c856d421f42226964374c6bbbaf687b
Javier Ferrándiz Fernández <96439507+javisys@users.noreply.github.com>
2023-08-01 19:02:01 +0200

    Update portDetect.sh

--- portDetect.sh
+++ portDetect.sh
@@ -6,7 +6,8 @@ if [ $1 ]; then
     ip=$1
     for port in $(seq 1 65535); do
         timeout 1 bash -c echo " '' > /dev/tcp/$ip/$port" 2> /dev/null && echo "Port $port - [OPEN]" &
-    done wait
+    done
+        wait
 else
     echo -e "\n[*]Usage: ./portDetect <ip-address>\n"
     exit 1
